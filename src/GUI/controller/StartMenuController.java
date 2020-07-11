fe98017b92a7430cc1c39c6158d5ab7d965a4737
Nicky <nxt930@student.bham.ac.uk>
2020-07-11 03:19:17 +0100

    creating additional folders into the GUI folder and making an initial start on an general menu

---
+++ src/GUI/controller/StartMenuController.java
@@ -0,0 +1,22 @@
+package GUI.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StartMenuController implements Initializable {
+
+    @FXML
+    private AnchorPane anchorBack;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+}
---
+++ src/GUI/scene/StartMenu.java
@@ -0,0 +1,39 @@
+package GUI.scene;
+
+import javafx.application.Application;
+import javafx.beans.InvalidationListener;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class StartMenu extends Application {
+
+
+    @Override
+    public void start(Stage initialStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/fxml/StartMenu.fxml"));
+
+
+        Scene scene = new Scene(root);
+        initialStage.setScene(scene);
+        initialStage.setTitle("Luggage System");
+        initialStage.show();
+
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
