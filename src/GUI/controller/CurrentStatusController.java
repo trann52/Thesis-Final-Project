e3f0db0e51ac576eb895383818af24cf55d34ce2
Nicky <nxt930@student.bham.ac.uk>
2020-07-17 23:01:11 +0100

    start new GUI

---
+++ src/GUI/controller/CurrentStatusController.java
@@ -0,0 +1,23 @@
+package GUI.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CurrentStatusController implements Initializable {
+
+    @FXML
+    private ProgressBar progressBar;
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
