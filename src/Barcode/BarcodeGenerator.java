c6bfcdd9e1e87906f264ce1301dcddc812bd18dc
Nicky <nxt930@student.bham.ac.uk>
2020-07-25 22:56:36 +0100

    update code and new class

--- src/Barcode/BarcodeGenerator.java
+++ src/Barcode/BarcodeGenerator.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 /**
  * This class has been created using the code found here:
  * https://github.com/primefaces/primefaces/blob/master/src/main/java/org/primefaces/application/resource/barcode/BarcodeGenerator.java
- * There are 4 authors: Thomas Andraschko, Cagatay Civici, mertsincan, Melloware
+ * There are 4 authors: Thomas Andraschko, Cagatay Civici, mertsincan and Melloware
  * This code uploaded to github on 16/06/2020.
  *
  * I have used this code because it will create a barcode generator.
