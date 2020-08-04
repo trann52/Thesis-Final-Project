111a41e05067bf58b8a6239df49bb233088ed75d
Nicky <nxt930@student.bham.ac.uk>
2020-08-04 16:39:37 +0100

    test class for trying out code

---
+++ src/TestCode.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class TestCode {
+
+    // generating a random  positive 10 digit number
+
+    public static void main(String[] args) { ;
+        for (int i =0; i<=5; i++){
+            System.out.println(Math.round(Math.random()*10000000000L));
+        }
+
+    }
+
+
+}
