6b53487f7378d567e732609baaaeb13ed0e5d958
Nicky <nxt930@student.bham.ac.uk>
2020-08-05 00:10:56 +0100

    new class

---
+++ src/Server/ServerAction.java
@@ -0,0 +1,20 @@
+package Server;
+
+import java.io.ObjectOutputStream;
+
+/**
+ * @author Nicky Tran
+ * @version 04/08/2020: 1.1
+ */
+public class ServerAction {
+
+    ObjectOutputStream out;
+
+    public ServerAction(ObjectOutputStream out) {
+        this.out = out;
+    }
+
+    /**
+     * -----------------------------------------------------------------------------------------------------------------
+     */
+}
